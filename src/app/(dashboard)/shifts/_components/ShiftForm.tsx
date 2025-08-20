021c1d266b66b85533adc8173742c599720d757a
C.Jay Martin <cjay.martin@gmail.com>
2025-08-20 17:20:33 -0400

    Tentative admin-notes fix

--- src/app/(dashboard)/shifts/_components/ShiftForm.tsx
+++ src/app/(dashboard)/shifts/_components/ShiftForm.tsx
@@ -214,6 +214,8 @@ export default function ShiftForm(props: ShiftFormProps) {
     const transformedShift = shift
         ? {
               ...shift,
+              notes: shift?.notes ?? '',
+              adminNotes: shift?.adminNotes ?? '',
               slots: shift?.slots || 1,
               // Parse dates from ISO format
               date: shift.startTime
