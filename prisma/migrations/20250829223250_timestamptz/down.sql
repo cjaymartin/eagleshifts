8c550dff3d264b91bc865e0de63ec02cf3d2e89a
C.Jay Martin <cjay.martin@gmail.com>
2025-09-05 20:52:28 -0400

    Oops I migrated again

--- prisma/migrations/20250829223250_timestamptz/down.sql
+++ prisma/migrations/20250829223250_timestamptz/down.sql
@@ -1,10 +1,10 @@
 -- Revert AlterTable for shift_draft
-ALTER TABLE "shift_draft" 
+ALTER TABLE "shift_draft"
 ALTER COLUMN "startTime" TYPE TIMESTAMP USING "startTime" AT TIME ZONE 'UTC',
 ALTER COLUMN "endTime" TYPE TIMESTAMP USING "endTime" AT TIME ZONE 'UTC';
 
 -- Revert AlterTable for shift
-ALTER TABLE "shift" 
+ALTER TABLE "shift"
 ALTER COLUMN "startTime" TYPE TIMESTAMP USING "startTime" AT TIME ZONE 'UTC',
 ALTER COLUMN "endTime" TYPE TIMESTAMP USING "endTime" AT TIME ZONE 'UTC';
 
